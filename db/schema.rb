58df4c9c64f9baae000d91f5d7b245a37634ee30
John Nelson <jrnelson75032@gmail.com>
2025-10-21 16:44:33 -0500

    Prepare for prod

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_10_19_040128) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -55,7 +58,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_19_040128) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_links_on_user_id"
   end
 
